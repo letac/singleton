0c6e9fb4315560fb0ab4c5fbfc73fdffe38e0b71
Strahinja Popovic <stralep@gmail.com>
2019-02-05 20:21:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# singleton
+
+Attempt to do [Efficient Programming with Components: Lecture 2 Part 1](https://www.youtube.com/watch?v=FUMPsmKnKv8), [Efficient Programming with Components: Lecture 2 Part 2](https://www.youtube.com/watch?v=B5yiLvaxPS4) and [Efficient Programming with Components: Lecture 3 Part 1](https://www.youtube.com/watch?v=sp_IBYVqMeQ&t=66s) from C++ to Ruts.
---
+++ src/lib.rs
@@ -0,0 +1,67 @@
+pub struct Singleton<T> {
+    value: T,
+}
+
+/// Conversion
+impl<T> Singleton<T> {
+    fn new(value: T) -> Self {
+        Self { value }
+    }
+}
+
+/// Conversion
+impl<T> From<T> for Singleton<T> {
+    fn from(value: T) -> Self {
+        Self { value }
+    }
+}
+
+/// Semi regular
+impl<T> Clone for Singleton<T>
+where
+    T: Clone,
+{
+    fn clone(&self) -> Self {
+        Self {
+            value: self.value.clone(),
+        }
+    }
+}
+
+/// Semi regular
+impl<T> Drop for Singleton<T> {
+    fn drop(&mut self) {}
+}
+
+/// Regular
+impl<T> PartialEq for Singleton<T>
+where
+    T: PartialEq,
+{
+    fn eq(&self, x: &Self) -> bool {
+        self.value.eq(&x.value)
+    }
+}
+
+/// Regular
+impl<T> Eq for Singleton<T> where T: Eq {}
+
+/// Totally-ordered
+impl<T> PartialOrd for Singleton<T>
+where
+    T: PartialOrd,
+{
+    fn partial_cmp(&self, x: &Self) -> Option<std::cmp::Ordering> {
+        self.value.partial_cmp(&x.value)
+    }
+}
+
+/// Totally-ordered
+impl<T> Ord for Singleton<T>
+where
+    T: Ord,
+{
+    fn cmp(&self, x: &Self) -> std::cmp::Ordering {
+        self.value.cmp(&x.value)
+    }
+}
